661c87c7939688d771961d0859fee6f708a3075d
Sherkhan <@sh.kurmanbekov>
2023-10-15 00:38:46 +0600

    Annotation based configuration and Lesson 14

--- src/main/java/org/example/database/pool/ConnectionPool.java
+++ src/main/java/org/example/database/pool/ConnectionPool.java
@@ -1,5 +1,7 @@
 package org.example.database.pool;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.InitializingBean;
 
 import java.util.List;
@@ -22,6 +24,7 @@ public class ConnectionPool implements InitializingBean {
         this.properties = properties;
     }
 
+    @PostConstruct
     private void init() {
         System.out.println("Init connection pool");
     }
@@ -31,6 +34,7 @@ public class ConnectionPool implements InitializingBean {
         System.out.println("Properties set");
     }
 
+    @PreDestroy
     private void destroy() {
         System.out.println("Clean connection pool");
     }
