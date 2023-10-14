6e2fa239da5167f4bae07ac13ff9460a9b8de080
Sherkhan <@sh.kurmanbekov>
2023-10-15 00:03:22 +0600

    BeanFactoryPostProcessor and Lesson 12

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,19 @@ import org.example.database.repository.CompanyRepository;
 import org.example.database.repository.UserRepository;
 import org.example.ioc.Container;
 import org.example.service.UserService;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 
 public class Main {
     public static void main(String[] args) {
+        String value = "Hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 //      clazz -> String -> Map<String, Object>
 //        System.out.println(context.getBean(ConnectionPool.class));
