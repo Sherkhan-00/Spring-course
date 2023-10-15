c92b74240550b1e9ff37440ecf5fe938f227a5f4
Sherkhan <@sh.kurmanbekov>
2023-10-15 12:46:52 +0600

    Custom BeanPostProcessor and Lesson 16

--- src/main/java/org/example/database/repository/CompanyRepository.java
+++ src/main/java/org/example/database/repository/CompanyRepository.java
@@ -1,15 +1,10 @@
 package org.example.database.repository;
 
+import org.example.bpp.InjectBean;
 import org.example.database.pool.ConnectionPool;
 
 public class CompanyRepository {
-    private final ConnectionPool connectionPool;
+    @InjectBean
+    private  ConnectionPool connectionPool;
 
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool){
-        return new CompanyRepository(connectionPool);
-    }
 }
