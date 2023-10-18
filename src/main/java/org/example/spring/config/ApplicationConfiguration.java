8d53addd366b2c0a49484ba997a81f48042e97a7
Sherkhan <@sh.kurmanbekov>
2023-10-18 13:07:23 +0600

    Bean and Lesson 26-27

--- src/main/java/org/example/spring/config/ApplicationConfiguration.java
+++ src/main/java/org/example/spring/config/ApplicationConfiguration.java
@@ -1,14 +1,18 @@
 package org.example.spring.config;
 
+import org.example.spring.database.pool.ConnectionPool;
 import org.example.spring.database.repository.CrudRepository;
+import org.example.spring.database.repository.UserRepository;
 import org.example.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
 
 //@ImportResource("classpath:application.xml")
 @Import(WebConfiguration.class)
-@Configuration
+@Configuration(proxyBeanMethods = true)
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "org.example.spring",
         useDefaultFilters = false,
@@ -19,4 +23,24 @@ import org.springframework.stereotype.Component;
         })
 public class ApplicationConfiguration {
 
+        @Bean("pool2")
+        @Scope(BeanDefinition.SCOPE_SINGLETON)
+        public ConnectionPool pool2(@Value("${db.username}") String userName) {
+                return new ConnectionPool(userName, 20);
+        }
+        @Bean
+        public ConnectionPool pool3() {
+                return new ConnectionPool("test-pool", 25);
+        }
+        @Bean
+        public UserRepository userRepository2(ConnectionPool pool2){
+                return new UserRepository(pool2);
+        }
+        @Bean
+        public UserRepository userRepository3(){
+                var connectionPool1 = pool3();
+                var connectionPool2 = pool3();
+                var connectionPool3 = pool3();
+                return new UserRepository(pool3());
+        }
 }
--- src/main/java/org/example/spring/database/repository/UserRepository.java
+++ src/main/java/org/example/spring/database/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package org.example.spring.database.repository;
 
 import org.example.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepository {
     private final ConnectionPool connectionPool;
 
-    public UserRepository(ConnectionPool connectionPool) {
+    public UserRepository(@Qualifier("pool2") ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
     }
 }
