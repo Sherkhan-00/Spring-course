fb6745222a35a5b12223671e6e84012b14d0cb73
Sherkhan <@sh.kurmanbekov>
2023-10-22 00:27:13 +0600

    Fix

--- src/main/java/org/example/spring/Main.java
+++ src/main/java/org/example/spring/Main.java
@@ -1,15 +1,7 @@
 package org.example.spring;
 
-import org.example.spring.config.ApplicationConfiguration;
-import org.example.spring.database.pool.ConnectionPool;
-import org.example.spring.database.repository.CrudRepository;
-import org.example.spring.service.CompanyService;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.io.Serializable;
 
 @SpringBootApplication
 public class Main {
