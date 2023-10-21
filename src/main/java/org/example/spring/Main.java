6838aa69e151d8b66ec523c74b5de54dde69de3d
Sherkhan <@sh.kurmanbekov>
2023-10-22 00:26:48 +0600

    SpringBoot configuration and Lesson 33

--- src/main/java/org/example/spring/Main.java
+++ src/main/java/org/example/spring/Main.java
@@ -5,30 +5,15 @@ import org.example.spring.database.pool.ConnectionPool;
 import org.example.spring.database.repository.CrudRepository;
 import org.example.spring.service.CompanyService;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.Serializable;
 
-
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        String value = "Hello";
-        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
-        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
-        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
-
-        try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web", "prod");
-            context.refresh();
-//      clazz -> String -> Map<String, Object>
-//        System.out.println(context.getBean(ConnectionPool.class));
-        var connectionPool = context.getBean("pool1", ConnectionPool.class);
-        System.out.println(connectionPool);
-
-        var companyService = context.getBean(CompanyService.class) ;
-        System.out.println(companyService.findById(1));
-
-        }
+        SpringApplication.run(Main.class, args);
     }
 }
