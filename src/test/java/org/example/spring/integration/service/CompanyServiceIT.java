aaace87d28dae1dc05e58904a8586b13c8d04082
Sherkhan <@sh.kurmanbekov>
2023-11-03 17:56:42 +0600

    Integration Testing and Lesson 43

---
+++ src/test/java/org/example/spring/integration/service/CompanyServiceIT.java
@@ -0,0 +1,43 @@
+package org.example.spring.integration.service;
+
+import org.example.spring.Main;
+import org.example.spring.database.entity.Company;
+import org.example.spring.dto.CompanyReadDto;
+import org.example.spring.listener.entity.EntityEvent;
+import org.example.spring.service.CompanyService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = Main.class, initializers = ConfigDataApplicationContextInitializer.class)
+@SpringBootTest
+public class CompanyServiceIT {
+
+    public static final Integer COMPANY_ID = 1;
+
+    @Autowired
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        var actualResult = companyService.findById(COMPANY_ID);
+
+        assertTrue(actualResult.isPresent());
+
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
+    }
+}
