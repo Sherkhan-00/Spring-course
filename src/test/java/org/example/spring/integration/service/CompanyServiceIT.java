e59329d17990d8cd6ad8de5e43a7894568996f9f
Sherkhan <@sh.kurmanbekov>
2023-11-14 18:51:35 +0600

    Inteegration Test and Lesson 44

--- src/test/java/org/example/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/org/example/spring/integration/service/CompanyServiceIT.java
@@ -1,23 +1,30 @@
 package org.example.spring.integration.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.spring.config.DatabaseProperties;
 import org.example.spring.dto.CompanyReadDto;
+import org.example.spring.integration.annotation.IT;
 import org.example.spring.service.CompanyService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = Main.class, initializers = ConfigDataApplicationContextInitializer.class)
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
 public class CompanyServiceIT {
 
     public static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    private CompanyService companyService;
+    private final CompanyService companyService;
+
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
